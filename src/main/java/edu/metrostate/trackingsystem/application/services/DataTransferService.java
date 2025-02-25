d6570bde2c0495a6ab9cd6951f9865ab703fbbf3
Bit2ByteGitHype <77911134+Bit2ByteGitHype@users.noreply.github.com>
2025-02-24 18:05:09 -0600

    Add PlantUML sequence diagram for importing .xml file, add notes and code for importXml() in DataTransferService.java

--- src/main/java/edu/metrostate/trackingsystem/application/services/DataTransferService.java
+++ src/main/java/edu/metrostate/trackingsystem/application/services/DataTransferService.java
@@ -2,6 +2,7 @@ package edu.metrostate.trackingsystem.application.services;
 
 import edu.metrostate.trackingsystem.infrastructure.logging.Logger;
 import edu.metrostate.trackingsystem.infrastructure.utils.JsonHandler;
+//import edu.metrostate.trackingsystem.infrastructure.utils.XmlHandler;
 import edu.metrostate.trackingsystem.infrastructure.utils.NotificationHandler;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -14,10 +15,16 @@ public class DataTransferService {
     private final NotificationHandler notificationHandler;
     private final JsonHandler jsonHandler;
 
+    //Note #1 - When using this I get an uninitialized error for xmlHandler AND jsonHandler
+    //private final XmlHandler xmlHandler;
+    //Which can be fixed in the DataTransferService constructor by adding , XmlHandler xmlHandler , but, I'm not sure if I should
+    //var dataTransferService = new DataTransferService(notificationHandler, jsonHandler); In main
+
     public DataTransferService(NotificationHandler notificationHandler, JsonHandler jsonHandler) {
         this.notificationHandler = notificationHandler;
         this.jsonHandler = jsonHandler;
     }
+
 
     public void importJson(Stage stage) {
         FileChooser fileChooser = new FileChooser();
@@ -59,7 +66,30 @@ public class DataTransferService {
         }
     }
 
+
     public void importXml(Stage stage) {
-        // TODO: needs an XML handler
-    }
+        // Adapted from importJson()
+        // see Note #1 in this file
+
+//        FileChooser fileChooser = new FileChooser();
+//        fileChooser.setTitle("Import XML File");
+//
+//
+//        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("XML Files (*.xml)", "*.xml");
+//        fileChooser.getExtensionFilters().add(extFilter);
+//
+//        File initialDir = new File("src/main/resources/files");
+//        if (initialDir.exists() && initialDir.isDirectory()) {
+//            fileChooser.setInitialDirectory(initialDir);
+//        }
+//
+//        File selectedFile = fileChooser.showOpenDialog(stage);
+//        if (selectedFile != null && xmlHandler.importFile(selectedFile)) {
+//            notificationHandler.notify("File imported.");
+//        } else if (selectedFile != null) {
+//            notificationHandler.notifyError("The import process failed. Ensure it is a valid XML file.");
+//        } else {
+//            logger.info("File selection cancelled.");
+//        }
+   }
 }
